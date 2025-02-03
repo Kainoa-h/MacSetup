1a46c09adc89b340fd9f552ac6b19e5e74650de9
Kainoa-h <kainoahowj@gmail.com>
2025-02-03 14:12:32 +0800

    added popup info for wifi widget. Still needs styling fixes

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -2,15 +2,18 @@
 
 haswifi="󰄍"
 nohasnowifi=""
-IPADDR="$(networksetup -getinfo Wi-Fi | grep "IP address" | head -n 1 | awk -F ':' '{print $2}')" # 0.0.0.0 | none
-
+PRIV_IPADDR="$(networksetup -getinfo Wi-Fi | grep "IP address" | head -n 1 | awk -F ':' '{print $2}')" # 0.0.0.0 | none
+PUB_IPADDR=""
 icon=""
 lbl=""
-if [ "$IPADDR" != " none" ]; then
+if [ "$PRIV_IPADDR" != " none" ]; then
     icon="$haswifi"
-    lbl="$IPADDR"
+    lbl="$PRIV_IPADDR"
+    PUB_IPADDR="$(curl https://ipinfo.io/ip)"
 else
     icon="$nohasnowifi"
 fi
 
-sketchybar --set "$NAME" label="$lbl" icon="$icon"
+sketchybar --set wifi icon="$icon"\
+    --set wifi.priv.addr label="$lbl" \
+    --set wifi.pub.addr label="$PUB_IPADDR"
