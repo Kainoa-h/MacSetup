c6a155fa7b53dd70b6a71bef46761bb301cd7124
Kainoa-h <kainoahowj@gmail.com>
2025-02-03 21:47:16 +0800

    Ran into some bugs that require new features added to aerospace or quite a bit of work to fix, gna try smth else

--- sketchybar/plugins/space_windows.sh
+++ sketchybar/plugins/space_windows.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-if [ "$SENDER" = "aerospace_workspace_change" ]; then
+#aerospace_workspace_change: tiggers when changing aerospace workspaces
+
+#space_windows_change: triggers when windows are opened or closed
+
+# This works fine, until an app is moved. Moving apps don't trigger space_windows_change since nothing is deleted or added.
+# So the labels never get redrawn.
+# Possible solutions:
+#   - Fork aerospace code and add a event for 'exec-on-window-moved', similar to the current 'exec-on-workspace-change'
+#   - Somehow detect app moved and do a `sketchybar --trigger`
+#   - Run through all spaces and update everything, everytime frontapp changes. very stupid & cursed but it should be easy. (Current soloution)
+#   - Maybe a lua refactor could fix this? Could maintain state of the json output of 'aerospace list-workspaces --monitor 1', and lazy update the diff?
+#   - ^ This feels possible in bash too.
+
+if [ "$SENDER" == "aerospace_workspace_change" ]; then
   prevapps=$(aerospace list-windows --workspace "$PREV_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-  if [ "${prevapps}" != "" ]; then
-    sketchybar --set space.$PREV_WORKSPACE drawing=on
-    icon_strip=""
-    while read -r app
-    do
-      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-    done <<< "${prevapps}"
-    sketchybar --set space.$PREV_WORKSPACE label="$icon_strip"
-  else
+  if [ "${prevapps}" == "" ]; then
     sketchybar --set space.$PREV_WORKSPACE drawing=off
   fi
 
-  apps=$(aerospace list-windows --workspace "$FOCUSED_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
   sketchybar --set space.$FOCUSED_WORKSPACE drawing=on
-  icon_strip=""
-  if [ "${apps}" != "" ]; then
-    while read -r app
-    do
-      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-    done <<< "${apps}"
-  else
-    icon_strip=""
-  fi
-  sketchybar --set space.$FOCUSED_WORKSPACE label="$icon_strip"
+
+elif [ "$SENDER" == "space_windows_change" ]; then
+  for sid in $(aerospace list-workspaces --monitor 1 --empty ); do
+      sketchybar --set space.$sid label=" "
+  done
+
+  for sid in $(aerospace list-workspaces --monitor 1 --empty no); do
+      apps=$(aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+      #sketchybar --set space.$sid drawing=on
+
+      icon_strip=" "
+      if [ "${apps}" != "" ]; then
+          while read -r app; do
+              icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+          done <<<"${apps}"
+      else
+          icon_strip=""
+      fi
+      sketchybar --set space.$sid label="$icon_strip"
+  done
 fi
