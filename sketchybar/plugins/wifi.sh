3de74e6721d5c01ac40a7e3cdc042bf4eeee6c81
Kainoa-h <kainoahowj@gmail.com>
2025-02-03 17:15:56 +0800

    changed wifi info to show on hover instead

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,19 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-haswifi="󰄍"
-nohasnowifi=""
-PRIV_IPADDR="$(networksetup -getinfo Wi-Fi | grep "IP address" | head -n 1 | awk -F ':' '{print $2}')" # 0.0.0.0 | none
-PUB_IPADDR=""
-icon=""
-lbl=""
-if [ "$PRIV_IPADDR" != " none" ]; then
-    icon="$haswifi"
-    lbl="$PRIV_IPADDR"
-    PUB_IPADDR="$(curl https://ipinfo.io/ip)"
-else
-    icon="$nohasnowifi"
+if [ "$SENDER" = "wifi_change" ] || [ "$SENDER" = "forced" ]; then
+    haswifi="󰄍"
+    nohasnowifi=""
+    PRIV_IPADDR="$(networksetup -getinfo Wi-Fi | grep "IP address" | head -n 1 | awk -F ':' '{print $2}')" # 0.0.0.0 | none
+    PUB_IPADDR=""
+    icon=""
+    lbl=""
+    if [ "$PRIV_IPADDR" != " none" ]; then
+        icon="$haswifi"
+        lbl="$PRIV_IPADDR"
+        PUB_IPADDR="$(curl https://ipinfo.io/ip)"
+    else
+        icon="$nohasnowifi"
+    fi
+
+    sketchybar --set wifi icon="$icon"\
+        --set wifi.priv.addr label="$lbl" \
+        --set wifi.pub.addr label=" $PUB_IPADDR"
+
+elif [ "$SENDER" == "mouse.entered" ]; then
+    sketchybar --set wifi popup.drawing=on
+elif [ "$SENDER" == "mouse.exited" ]; then
+    sketchybar --set wifi popup.drawing=off
 fi
-
-sketchybar --set wifi icon="$icon"\
-    --set wifi.priv.addr label="$lbl" \
-    --set wifi.pub.addr label="$PUB_IPADDR"
