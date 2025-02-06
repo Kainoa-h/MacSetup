2ad21d3e12bd2a35b54e622f0581e201601b94ff
Kainoa-h <kainoahowj@gmail.com>
2025-02-06 09:05:14 +0800

    added colours to bat widget

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+source "$CONFIG_DIR/plugins/colors.sh"
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
@@ -8,14 +9,22 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
-  ;;
-  [6-8][0-9]) ICON=""
-  ;;
-  [3-5][0-9]) ICON=""
-  ;;
-  [1-2][0-9]) ICON=""
-  ;;
+  9[0-9]|100)
+    ICON=""
+    COLOUR=$TEXT_GREY
+    ;;
+  [6-8][0-9]) 
+    ICON=""
+    COLOUR=$TEXT_GREY
+    ;;
+  [3-5][0-9]) 
+    ICON=""
+    COLOUR=$TEXT_ORGANGE
+    ;;
+  [1-2][0-9]) 
+    ICON=""
+    COLOUR=$TEXT_RED
+    ;;
   *) ICON=""
 esac
 
@@ -25,4 +34,4 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" label.color=$COLOUR icon.color=$COLOUR
