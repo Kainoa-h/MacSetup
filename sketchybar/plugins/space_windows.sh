b2f7ce307ee00a7b9e7464a8a57cb8347c27abfe
Kainoa-h <kainoahowj@gmail.com>
2025-05-14 15:46:24 +0800

    Workspace now follows when a window is moved.

--- sketchybar/plugins/space_windows.sh
+++ sketchybar/plugins/space_windows.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Interim solution. See branch 'Space'.
-
-for sid in $(aerospace list-workspaces --monitor 1 --empty ); do
-    sketchybar --set space.$sid label=" " drawing=off
-done
-
-for sid in $(aerospace list-workspaces --monitor 1 --empty no); do
-    apps=$(aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-    sketchybar --set space.$sid drawing=on
-
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  prevapps=$(aerospace list-windows --workspace "$PREV_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+  if [ "${prevapps}" != "" ]; then
+    sketchybar --set space.$PREV_WORKSPACE drawing=on
     icon_strip=" "
-    if [ "${apps}" != "" ]; then
-        while read -r app; do
-            icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-        done <<<"${apps}"
-    else
-        icon_strip=""
-    fi
-    sketchybar --set space.$sid label="$icon_strip"
-done
-
-if [ "$SENDER" == "aerospace_workspace_change" ]; then
-  sketchybar --set space.$FOCUSED_WORKSPACE drawing=on
+    while read -r app
+    do
+      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+    done <<< "${prevapps}"
+    sketchybar --set space.$PREV_WORKSPACE label="$icon_strip"
+  else
+    sketchybar --set space.$PREV_WORKSPACE drawing=off
+  fi
+else
+  FOCUSED_WORKSPACE="$(aerospace list-workspaces --focused)"
 fi
+
+apps=$(aerospace list-windows --workspace "$FOCUSED_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+sketchybar --set space.$FOCUSED_WORKSPACE drawing=on
+icon_strip=" "
+if [ "${apps}" != "" ]; then
+  while read -r app
+  do
+    icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+  done <<< "${apps}"
+else
+  icon_strip=""
+fi
+sketchybar --set space.$FOCUSED_WORKSPACE label="$icon_strip"
