ff2f788bf34597b10f1ce84d819504ed7eea7663
Kainoa-h <kainoahowj@gmail.com>
2025-05-19 14:39:07 +0800

    buncha small changes idk i'm lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+end
