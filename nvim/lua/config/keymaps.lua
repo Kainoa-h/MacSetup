66b80d2e48963778748aaa2316a472c448de2155
Kainoa-h <kainoahowj@gmail.com>
2025-05-19 22:08:39 +0800

    new lazygit config!

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+
+vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
+
+local tele_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ds", tele_builtin.lsp_document_symbols, { desc = "Document Symbols" })
