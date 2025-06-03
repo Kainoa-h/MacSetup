a3f77e76bc1c1cf445f1873062609c3561ba1671
Kainoa-h <kainoahowj@gmail.com>
2025-06-03 22:26:19 +0800

    fix: invalid sourcing of colors.sh

--- sketchybar/plugins/ncspot.sh
+++ sketchybar/plugins/ncspot.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-source "$PLUGIN_DIR/colors.sh"
+source "$CONFIG_DIR/plugins/colors.sh"
 
 JSON="$(nc -U /tmp/ncspot-501/ncspot.sock </dev/null 2>/dev/null)"
 
 # Check if the JSON is empty
 if [ -z "$JSON" ]; then
-  sketchybar --set ncspot drawing=false
+  sketchybar --set ncspot drawing=off
   exit 0
 fi
-
-sketchybar --set ncspot drawing=true
 
 STATUS="$(echo "$JSON" | jq -r '.mode.Playing')"
 
 if [ "$STATUS" = "null" ]; then
-  sketchybar --set ncspot icon.color="$TEXT_GREY"
+  sketchybar --set ncspot drawing=on icon.color="$TEXT_GREY"
   exit 0
 fi
 
 TITLE="$(echo "$JSON" | jq -r '.playable.title')"
-sketchybar --set ncspot label="$TITLE" icon.color="$TEXT_SPOTIFY_GREEN"
+sketchybar --set ncspot drawing=on label="$TITLE" icon.color="$TEXT_SPOTIFY_GREEN"
