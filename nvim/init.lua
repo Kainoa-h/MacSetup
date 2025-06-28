dfe50d45b5438629365ced93af715442393ed5f2
Kainoa-h <kainoahowj@gmail.com>
2025-06-28 16:55:54 +0900

    add: default indent method now uses treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,6 @@ vim.o.background = "dark"
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
 vim.cmd("colorscheme gruvbox-material")
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
