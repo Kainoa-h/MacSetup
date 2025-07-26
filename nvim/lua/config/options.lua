8112ae36dcc87822244a9b0d8aa30e11145df0ea
Kainoa-h <kainoahowj@gmail.com>
2025-07-26 19:06:15 +0900

    add: improved neovide config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,4 +9,5 @@ if vim.g.neovide then
   vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
   vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
   vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
+  vim.g.neovide_hide_mouse_when_typing = true
 end
