b20d96d2fe3992072dfe0d09cb591fb52834f457
Kainoa-h <kainoahowj@gmail.com>
2025-10-19 10:25:15 +0800

    feat(yazi): added qol plugins

---
+++ yazi/init.lua
@@ -0,0 +1,5 @@
+-- dedukun/relative-motions
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
+
+-- Rolv-Apneseth/starship
+require("starship"):setup()
