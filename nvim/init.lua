9cfe85d810ca90b1c0854be04d44f1e2219bf038
Kainoa-h <kainoahowj@gmail.com>
2025-11-10 03:28:56 +0800

    feat(nvim): new theme!

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,7 @@ require("config.lazy")
 vim.o.background = "dark"
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
-vim.cmd("colorscheme gruvbox-material")
+-- vim.cmd("colorscheme gruvbox-material")
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
