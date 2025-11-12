a40bc2ec07e85e95f89ced436f3424c6996546cf
Kainoa-h <kainoahowj@gmail.com>
2025-11-13 02:25:50 +0800

    fix(sketchybar): fix <redacted> wifi ssid in Sequoia 15.7.2
    fix found on https://github.com/fastfetch-cli/fastfetch/issues/1874

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -4,7 +4,7 @@ if [ "$SENDER" = "wifi_change" ] || [ "$SENDER" = "forced" ]; then
     haswifi="󰄍"
     nohasnowifi=""
     PRIV_IPADDR="$(networksetup -getinfo Wi-Fi | grep "IP address" | head -n 1 | awk -F ':' '{print $2}')" # 0.0.0.0 | none
-    SSID="$(system_profiler SPAirPortDataType | awk '/Current Network Information:/ { getline; print substr($0, 13, (length($0) - 13)); exit }')"
+    SSID="$(networksetup -listpreferredwirelessnetworks en0 | sed -n '2s/^\t//p')"
     icon=""
     if [ "$PRIV_IPADDR" != " none" ]; then
         icon="$haswifi"
